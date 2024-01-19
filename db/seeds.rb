27eccec4b74712c6b0c6ca3f2ad50754bd2b19fe
Gyuchan <gyuchantech@gmail.com>
2024-01-19 14:55:50 +1300

    add post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Post.destroy_all
+
+# Create 2 posts
+2.times do
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    body: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+end
