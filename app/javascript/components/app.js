1583bc0c949b1a125cc46ad0f08306baa2967d4b
Gyuchan <gyuchantech@gmail.com>
2024-01-20 22:48:48 +1300

    setup vite react app

---
+++ app/javascript/components/app.js
@@ -0,0 +1,5 @@
+console.log(process.env.NODE_ENV) // dev
+console.log(process.env.NODE_ENV === 'dev') // false
+console.log(process.env.NODE_ENV.length) // 4 (including a space at the end) console.log(process.env.NODE_ENV) // dev
+console.log(process.env.NODE_ENV === 'dev') // false
+console.log(process.env.NODE_ENV.length) // 4 (including a space at the end) 
