72d12ffbef018f2e7d6c15b75a43da13ede30db4
Gyuchan <gyuchantech@gmail.com>
2024-01-21 15:36:30 +1300

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <h1>React on Rails Blog</h1>
         <p>Find this application layout in client/src/App.jsx</p>
         <NavBar />
+        
         <AppRoutes />
       </div>
     </Router>
